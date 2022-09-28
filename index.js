50b5598adb910c181af9ee10541863856d396608
shaantanu9 <shantanubombatkar2@gmail.com>
2022-09-28 16:07:16 +0530

    Added Json-Server db.json and Other files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const data = require('./db.json');
+const data = require("./db.json");
 
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
@@ -8,4 +8,6 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => console.log(`JSON Server is running on port ${port}`));
+server.listen(port, () =>
+  console.log(`JSON Server is running on port ${port}`)
+);
